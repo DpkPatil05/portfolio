84f874bfbcb154f7289c197e7c8903cb3eea106b
Deepak <patildeepak188@gmail.com>
2020-11-15 21:05:44 +0530

    new

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = (props.) => {
+    return(
+        <nav className="navbar">
+            <ul className="navbar-nav">
+                { props.children }
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
