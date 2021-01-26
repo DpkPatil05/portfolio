991d4ab64a33ea5d352a90d72b4ec9ebd7bd6ccc
DpkPatil05 <patildeepak188@gmail.com>
2021-01-26 14:39:04 +0530

    Page scroll complete

--- js/functions.js
+++ js/functions.js
@@ -6,9 +6,27 @@ let parallax = new Parallax(scene);
 // Page switch //
 let loadedCount = 0;
 let loadedPages = ['#indexContainer'];
-let firstLoad = true;
 
-function loadPage(directory, containerName, reverseScroll) {
+function scrollToPage(containerName, loadedCount, reverseScroll) {
+    let pageVal = 0;
+    reverseScroll?pageVal=-1:pageVal=1;
+    let index = loadedCount+pageVal;
+
+    $(containerName)
+        .addClass("hidden")
+        .fadeOut('fast', function(){
+            $(this).hide(); //hide old container
+    });
+
+    $(loadedPages[index])
+        .removeClass("hidden")
+        .fadeIn('fast', function(){ //show new container
+            $(this).show();
+    });
+    return;
+}
+
+function loadPage(directory, loadContainer, reverseScroll) {
     $.get(directory, function(data){ 
         if(!reverseScroll){
             $(data).slideUp('normal', function() {
@@ -20,47 +38,35 @@ function loadPage(directory, containerName, reverseScroll) {
             });
         }
 
-        if(!loadedPages.includes(containerName)) {
-            loadedPages.push(containerName);
+        if(!loadedPages.includes(loadContainer)) {
+            loadedPages.push(loadContainer);
         }
         // $("#loadingAnimation1").hide();
     });
     return;
 }
 
-function scrollToPage(containerName, loadedCount, reverseScroll) {
-    let pageVal = 0;
-    reverseScroll?pageVal=-1:pageVal=1;
-    let index = loadedCount+pageVal;
-
-    $(containerName)
-    .addClass("hidden")
-    .fadeOut('fast', function(){
-        $(this).hide(); //hide old container
-    });
-
-    $(loadedPages[index])
-    .removeClass("hidden")
-    .fadeIn('fast', function(){ //show new container
-        $(this).show();
-    });
-    return;
-}
-
-function showData(filename, containerName, loadedCount, reverseScroll) {
+function showData(filename, containerName, loadContainer, loadedCount, reverseScroll) {
     let directory = '/'+filename;
+    let i = 0,  loaded=false;
 
-    if(containerName == "#indexContainer" && firstLoad) {
-        loadPage(directory, containerName, reverseScroll);
+    //Check if loaded
+    if(loadedPages[loadedCount] == loadContainer && !reverseScroll){
+        loaded = true;
+    }
+    if(loadedPages[loadedCount-1] == loadContainer && reverseScroll){
+        loaded = true; 
+    }
+
+    console.log(loadedPages[loadedCount] + "on index " + loadedCount);
+    console.log("Loaded: " + loaded);
+
+    if(loaded && reverseScroll) {
         scrollToPage(containerName, loadedCount, reverseScroll);
-        firstLoad != firstLoad;
-    } else if(loadedPages.includes(containerName) && reverseScroll) {
-        scrollToPage(containerName, loadedCount, reverseScroll);
-    } else if(loadedPages.includes(containerName) && containerName != "#indexContainer"
-     && !reverseScroll) {
+    } else if(loaded && !reverseScroll) {
         scrollToPage(containerName, loadedCount, reverseScroll);
     } else {
-        loadPage(directory, containerName, reverseScroll);
+        loadPage(directory, loadContainer, reverseScroll);
         scrollToPage(containerName, loadedCount, reverseScroll);
     }
     return;
@@ -74,18 +80,22 @@ $('window, body, object').on('mousewheel', function () {
             //scroll down
             if(loadedCount == 0) {
                 // $("#loadingAnimation1").show();
-                showData("works.html", "#indexContainer", loadedCount, false);
+                showData("works.html", "#indexContainer", "#worksContainer", loadedCount+1, false);
                 loadedCount++;
                 return;
             } 
             if(loadedCount == 1) {
-                showData("about-me.html", "#worksContainer", loadedCount, false);
+                showData("about-me.html", "#worksContainer", "#aboutContainer", loadedCount+1, false);
                 loadedCount++;
                 return;
             }
             if(loadedCount == 2) {
-                showData("contact-me.html", "#aboutContainer", loadedCount, false);
+                showData("contact-me.html", "#aboutContainer", "#contactContainer",loadedCount+1, false);
                 loadedCount++;
+                return;
+            }
+            if(loadedCount >= 3) {
+                loadedCount = 3;
                 return;
             }
         } else {
@@ -94,18 +104,18 @@ $('window, body, object').on('mousewheel', function () {
                 loadedCount = 0;
                 return;
             }
-            if(loadedCount == 1) {
-                showData("index-load.html", "#worksContainer", loadedCount, true);
+            if (loadedCount == 1) {
+                showData("", "#worksContainer", "#indexContainer", loadedCount, true);
                 loadedCount--;
                 return;
             }
             if(loadedCount == 2) {
-                showData("works.html", "#aboutContainer", loadedCount, true);
+                showData("works.html", "#aboutContainer", "#worksContainer", loadedCount, true);
                 loadedCount--;
                 return;
             }
             if(loadedCount == 3) {
-                showData("about-me.html", "#contactContainer", loadedCount, true);
+                showData("about-me.html", "#contactContainer", "#aboutContainer", loadedCount, true);
                 loadedCount--;
                 return;
             }
